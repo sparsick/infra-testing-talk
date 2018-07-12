a8e910cf85db76f4108fde23e41fc683b4d0f31b
sparsick <sparsick@web.de>
2018-07-12 16:50:52 +0200

    init setup spring boot app

---
+++ infra-testing-demo-app/src/main/java/com/github/sparsick/infra/testing/infratestingdemoapp/InfraTestingDemoAppApplication.java
@@ -0,0 +1,12 @@
+package com.github.sparsick.infra.testing.infratestingdemoapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfraTestingDemoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InfraTestingDemoAppApplication.class, args);
+	}
+}
