ff54e3587b7990f74d5596a78303f3c14c11125d
sparsick <sparsick@web.de>
2018-07-19 16:59:41 +0200

    add testcontainer database example

---
+++ infra-testing-demo-app/src/test/java/com/github/sparsick/infra/testing/infratestingdemoapp/database/PersonRepositoryTest.java
@@ -0,0 +1,54 @@
+package com.github.sparsick.infra.testing.infratestingdemoapp.database;
+
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import org.flywaydb.core.Flyway;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.testcontainers.containers.PostgreSQLContainer;
+
+import java.sql.SQLException;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+public class PersonRepositoryTest {
+
+    @Rule
+    public PostgreSQLContainer postgres = new PostgreSQLContainer();
+
+    private PersonRepository repositoryUnderTest;
+
+    @Before
+    public void setup(){
+        HikariConfig hikariConfig = new HikariConfig();
+        hikariConfig.setJdbcUrl(postgres.getJdbcUrl());
+        hikariConfig.setUsername(postgres.getUsername());
+        hikariConfig.setPassword(postgres.getPassword());
+
+        HikariDataSource ds = new HikariDataSource(hikariConfig);
+        Flyway flyway = new Flyway();
+        flyway.setDataSource(ds);
+        flyway.migrate();
+
+        repositoryUnderTest = new PersonRepository(ds);
+    }
+
+    @Test
+    public void saveAndFindAllPerson() throws SQLException {
+       Person person = new Person();
+       person.setFirstName("firstName");
+       person.setLastName("lastName");
+       person.setJobTitle("jobTitle");
+
+       repositoryUnderTest.save(person);
+
+        List<Person> persons = repositoryUnderTest.findAllPersons();
+        assertThat(persons).hasSize(1).contains(person);
+
+
+    }
+}
