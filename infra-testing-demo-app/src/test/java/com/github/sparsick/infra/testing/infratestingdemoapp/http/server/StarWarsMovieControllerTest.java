3ad52979a0326c0a07db92e6395f2cefa0c308b0
sparsick <sparsick@web.de>
2018-07-31 21:23:44 +0200

    add example for API logic test

--- infra-testing-demo-app/src/test/java/com/github/sparsick/infra/testing/infratestingdemoapp/http/server/StarWarsMovieControllerTest.java
+++ infra-testing-demo-app/src/test/java/com/github/sparsick/infra/testing/infratestingdemoapp/http/server/StarWarsMovieControllerTest.java
@@ -1,29 +1,22 @@
 package com.github.sparsick.infra.testing.infratestingdemoapp.http.server;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.servlet.MockMvc;
 
-import static io.restassured.module.mockmvc.RestAssuredMockMvc.given;
-import static org.hamcrest.CoreMatchers.hasItems;
+import org.junit.jupiter.api.Test;
 
-@RunWith(SpringRunner.class)
-@WebMvcTest(StarWarsMovieController.class)
-public class StarWarsMovieControllerTest {
+import java.util.List;
 
-    @Autowired
-    private MockMvc mockMvc;
+import static org.assertj.core.api.Assertions.assertThat;
+
+class StarWarsMovieControllerTest {
 
     @Test
-    public void findAllMovies(){
-        given().mockMvc(mockMvc).
-        when().get("/starwars/movies").
-        then().statusCode(200)
-                .body("title",  hasItems("A New Hope", "The Force Awakens"))
-                .body("director", hasItems("George Lucas", "J. J. Abrams"));
-    }
+    void findAllMovies(){
+        StarWarsMovieController controllerUnderTest = new StarWarsMovieController();
+        List<StarWarsMovie> movies = controllerUnderTest.findAllMovies();
 
+        assertThat(movies)
+                .hasSize(2)
+                .extracting("director")
+                .containsExactlyInAnyOrder( "George Lucas","J. J. Abrams" );
+    }
 }
