1db513e2028c46d6bd7af4a078e4b97738125710
sparsick <sparsick@web.de>
2018-11-07 10:56:11 +0100

    add example for testcontainers with junit5

---
+++ infra-testing-demo-app/src/test/java/com/github/sparsick/infra/testing/infratestingdemoapp/database/PersonRepositoryJUnit5Test.java
@@ -0,0 +1,51 @@
+package com.github.sparsick.infra.testing.infratestingdemoapp.database;
+
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import org.flywaydb.core.Flyway;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.testcontainers.containers.PostgreSQLContainer;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+//@Testcontainers
+class PersonRepositoryJUnit5Test {
+
+//    @Container
+    private PostgreSQLContainer postgres = new PostgreSQLContainer();
+
+    private PersonRepository repositoryUnderTest;
+
+    @BeforeEach
+    public void setup(){
+        HikariConfig hikariConfig = new HikariConfig();
+        hikariConfig.setJdbcUrl(postgres.getJdbcUrl());
+        hikariConfig.setUsername(postgres.getUsername());
+        hikariConfig.setPassword(postgres.getPassword());
+
+        HikariDataSource ds = new HikariDataSource(hikariConfig);
+        Flyway flyway = new Flyway();
+        flyway.setDataSource(ds);
+        flyway.migrate();
+
+        repositoryUnderTest = new PersonRepository(ds);
+    }
+
+    @Test
+    void saveAndFindAllPerson() {
+       Person person = new Person();
+       person.setFirstName("firstName");
+       person.setLastName("lastName");
+       person.setJobTitle("jobTitle");
+
+       repositoryUnderTest.save(person);
+
+        List<Person> persons = repositoryUnderTest.findAllPersons();
+        assertThat(persons).hasSize(1).contains(person);
+    }
+}
