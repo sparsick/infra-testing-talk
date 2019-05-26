3297ff3ce27f77e1240ee478d822bd7fc90d3921
sparsick <sparsick@web.de>
2019-05-27 00:09:39 +0200

    add plain mockmvc example

--- infra-testing-demo-app/src/test/java/com/github/sparsick/infra/testing/infratestingdemoapp/http/server/StarWarsMovieControllerITest.java
+++ infra-testing-demo-app/src/test/java/com/github/sparsick/infra/testing/infratestingdemoapp/http/server/StarWarsMovieControllerITest.java
@@ -9,10 +9,13 @@ import org.springframework.test.web.servlet.MockMvc;
 
 import static io.restassured.module.mockmvc.RestAssuredMockMvc.given;
 import static org.hamcrest.CoreMatchers.hasItems;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(StarWarsMovieController.class)
-public class StarWarsMovieControllerIT {
+public class StarWarsMovieControllerITest {
 
     @Autowired
     private MockMvc mockMvc;
@@ -24,6 +27,17 @@ public class StarWarsMovieControllerIT {
         then().statusCode(200)
                 .body("title",  hasItems("A New Hope", "The Force Awakens"))
                 .body("director", hasItems("George Lucas", "J. J. Abrams"));
+    }
+
+    @Test
+    public void findAllMovies_plainMockMvc() throws Exception {
+        mockMvc.perform(get("/starwars/movies"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("[0].title").value("A New Hope"))
+                .andExpect(jsonPath("[0].director").value("George Lucas"))
+                .andExpect(jsonPath("[1].title").value("The Force Awakens"))
+                .andExpect(jsonPath("[1].director").value("J. J. Abrams"));
+
     }
 
 }
