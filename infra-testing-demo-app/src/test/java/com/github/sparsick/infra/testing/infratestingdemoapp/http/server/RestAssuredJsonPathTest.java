2a46097420003d9506c774c323651da153ac3e6a
sparsick <sparsick@web.de>
2019-11-30 13:10:42 +0100

    add sample for rest assured jsonpath and xmlpath

---
+++ infra-testing-demo-app/src/test/java/com/github/sparsick/infra/testing/infratestingdemoapp/http/server/RestAssuredJsonPathTest.java
@@ -0,0 +1,44 @@
+package com.github.sparsick.infra.testing.infratestingdemoapp.http.server;
+
+import io.restassured.path.xml.element.NodeChildren;
+import org.apache.commons.io.IOUtils;
+import org.junit.jupiter.api.Test;
+import org.springframework.core.io.ClassPathResource;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.Charset;
+import java.util.List;
+
+import static io.restassured.path.json.JsonPath.from;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class RestAssuredJsonPathTest {
+
+    @Test
+    void demoSimpleElement() throws Exception {
+        String json = loadJson("starwars-testdata/one-starship-object.json");
+
+        String starShipName = from(json).get("starship.name");
+
+        assertThat(starShipName).isEqualTo("Slave 1");
+
+    }
+
+    @Test
+    void demoArray() throws Exception {
+        String json = loadJson("starwars-testdata/one-starship-object.json");
+
+        List<String> allFilmTitles = from(json).get("starship.films.title");
+
+        assertThat(allFilmTitles).hasSize(2);
+
+    }
+
+    private String loadJson(String jsonPath) throws IOException {
+        try (InputStream inputStream = new ClassPathResource(jsonPath).getInputStream()) {
+            return IOUtils.toString(inputStream, Charset.defaultCharset());
+        }
+    }
+
+}
---
+++ infra-testing-demo-app/src/test/java/com/github/sparsick/infra/testing/infratestingdemoapp/http/server/RestAssuredXmlPathTest.java
@@ -0,0 +1,50 @@
+package com.github.sparsick.infra.testing.infratestingdemoapp.http.server;
+
+import io.restassured.path.xml.element.Node;
+import io.restassured.path.xml.element.NodeChildren;
+import org.apache.commons.io.IOUtils;
+import org.junit.jupiter.api.Test;
+import org.springframework.core.io.ClassPathResource;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.Charset;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static io.restassured.path.xml.XmlPath.from;
+import static java.util.stream.Collectors.*;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class RestAssuredXmlPathTest {
+
+    @Test
+    void demoSimpleElement() throws Exception {
+        String xml = loadXml("starwars-testdata/one-starship-object.xml");
+
+        String starShipModel = from(xml).get("starship.model");
+
+        assertThat(starShipModel).isEqualTo("Firespray-31-class patrol and attack");
+    }
+
+    @Test
+    void demoArray() throws Exception {
+        String json = loadXml("starwars-testdata/one-starship-object.xml");
+
+       List<String> allFilmDirector = ((NodeChildren)from(json).get("starship.films.film.director"))
+                                            .list()
+                                            .stream()
+                                            .map(Node::value)
+                                            .collect(toList());
+
+
+        assertThat(allFilmDirector).hasSize(2).contains("George Lucas", "Irvin Kershner");
+    }
+
+    private String loadXml(String jsonPath) throws IOException {
+        try (InputStream inputStream = new ClassPathResource(jsonPath).getInputStream()) {
+            return IOUtils.toString(inputStream, Charset.defaultCharset());
+        }
+    }
+
+}
